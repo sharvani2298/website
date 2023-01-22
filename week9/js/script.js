18d4a68001686c0f3a1e72e6e02dbd91f35f1b42
sharvani shrestha <np03cs4a220165@heraldcollege.edu.np>
2023-01-22 13:43:21 +0545

    Added Javascript

---
+++ week9/js/script.js
@@ -0,0 +1,17 @@
+console.log("javascript");// identifies error and tells us how js works
+alert("bla bla bla");
+console.log(document.getElementById("paragraph").innerHTML);
+document.getElementById("paragraph").innerHTML="<h1>hello world!</h1>";
+document.getElementById("paragraph").style.cssText="colour:red;background:green";
+var age = 10;
+//age = 56;
+const ag = 10;
+//ag= 73;
+let a = 10;
+console.log(age);   
+console.log(ag);
+console.log(a);
+var name= "jenny";
+console.log(name);
+
+
